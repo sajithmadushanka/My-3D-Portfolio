3a81693b81f2887a8670adae55ddf50b59625e00
sajeebro <sajithmadusanka720@gmail.com>
2023-12-30 01:17:18 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsC
 const App = ()=>{
   return (
        <BrowserRouter>
-         <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <div className='relative z-0 bg-primary'>
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
+        <About />
+        <Experience />
+        <Tech />
         <Works />
         <Feedbacks />
         <div className='relative z-0'>
